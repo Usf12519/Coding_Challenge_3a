9e4e5a17b02890e8a6d1568767af91aa5216584d
Cameron Smith <Cs11@usf.edu>
2025-09-14 12:50:00 -0400

    Create cc_3.js

---
+++ cc_3.js
@@ -0,0 +1,29 @@
+// Step 1: Create a New JavaScript File 
+
+//Step 2: Create Customer Records
+
+let customers = [
+    {'name: Hannah , email: hannah1234@gmail.com , purchases: one xxl shirt'};
+    {'name: Maddie , email: maddie2345@yahaoo.com , purchases: two large shorts'};
+    {'name: Bob , email: bob9876@gmail.com , purchases: one small shirt'};
+];
+
+// Step 3: Add and Remove Data
+
+console.log.push(Paul)
+console.log.shift('name: Hannah , email: hannah1234@gmail.com , purchases: one xxl shirt')
+
+// Step 4: Update Customer Info
+
+customer.findOneAndUpdate(
+    {$set: {email: "paul1234@ggmail.com"}};
+    {new: true}
+)
+
+// Step 5: Display Customer Information
+
+console.log(customers.forEach(
+    {'name:hannah , email: hannah1234@gmail.com , purchases: one xxl shirt'}
+    {'name: maddie , email: maddie2345@gmail.com , purchases: two large shorts'}
+    {'name: Bob , email: bob9876@gmail.com , purchases: one small shirt'}
+));
